1383ff748e3ba8fe6b9921aa9747a43ae91d76fa
GBRNF <nf_pts_senior_engineer1@mail.ru>
2024-06-16 15:18:52 +0500

    Навожу марафет в коде

--- GameManager.cs
+++ GameManager.cs
@@ -1,78 +1,91 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+
 namespace _15shkiNew
 {
     internal class GameManager
     {
+        private string Hello = "Приветствую в игре \"Пятнашки\" v 1.1";
+
+        private string StarMenu = "1 - Старт игры\n2 - Рекорды\n3 - Создатель\n0 - Выход";
+
         private int ReadKey()
         {
             ConsoleKeyInfo key;
             key = Console.ReadKey(true);
             return (int)key.KeyChar;
         }
+
         private void ReturnStartMenu()
         {
             Console.WriteLine("Нажмите любую клавишу для выхода в главное меню");
             Console.ReadKey(true);
             Console.Clear();
         }
-        private string Hello = "Приветствую в игре \"Пятнашки\" v 1.1";
-        private string StarMenu = "1 - Старт игры\n2 - Рекорды\n3 - Создатель\n0 - Выход";
+
+        private string SetNickName (string nickName)
+        {
+            return nickName;
+        }
 
         static void Main(string[] args)
         {
             Console.Title = "Пятнашки";
-            bool endGame = false;
+            bool exitGame = false;
             GameManager gameManager = new GameManager();
-            while (endGame == false)
+            Console.WriteLine(gameManager.Hello);
+            Console.WriteLine("Введите выше имя");
+            GameStat gameStat = new GameStat(gameManager.SetNickName("Test"));
+            while (!exitGame)
             {
-                Console.WriteLine(gameManager.Hello);
+                Console.WriteLine($"Приветствую,{gameStat.NickName}");
                 Console.WriteLine(gameManager.StarMenu);
                 switch (gameManager.ReadKey())
                 {
                     case 49: // Старт
-                    GameField gameField = new GameField(); // тут создаю экземпляр класса
-                    Console.WriteLine("Упарвление: 8 - вверх, 5 - вниз, 4 - влево, 6 - вправо или W/A/S/D");
-                    int readKey;
-                    while (true) // создаю замкнутый цикл
-                    {
-                        readKey = gameManager.ReadKey();
-                        if (readKey != 1081 && readKey != 113) // Q или Й для выхода
+                        GameField gameField = new GameField(); // тут создаю экземпляр класса
+                        Console.WriteLine("Упарвление: 8 - вверх, 5 - вниз, 4 - влево, 6 - вправо или W/A/S/D");
+                        int readKey;
+                        while (true) // создаю замкнутый цикл
                         {
-                            gameField.MoveItem(readKey);
-                            Console.Clear();
-                            gameField.Show();
-                            Console.WriteLine("Нажать \"Q\" для выхода в главное меню");
-                            if (gameField.GameOver() == true)
+                            readKey = gameManager.ReadKey();
+                            if (readKey != 1081 && readKey != 113) // Q или Й для выхода
+                            {
+                                gameField.MoveItem(readKey);
+                                Console.Clear();
+                                gameField.Show();
+                                Console.WriteLine("Нажать \"Q\" для выхода в главное меню");
+                                if (gameField.GameOver() == true)
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("Винер винер чикен динер :D");
+                                    Console.WriteLine("Press any key...");
+                                    Console.ReadLine();
+                                    break;
+                                }
+                            }
+                            else
                             {
                                 Console.Clear();
-                                Console.WriteLine("Винер винер чикен динер :D");
-                                Console.WriteLine("Press any key...");
-                                Console.ReadLine();
                                 break;
                             }
                         }
-                        else
-                        {
-                            Console.Clear();
-                            break;
-                        }
-                    }
-                    break;
+                        break;
                     case 50: // Рекорды
-                    Console.Clear();
-                    Console.WriteLine("Тут пока не чего нет\n");
-                    gameManager.ReturnStartMenu();
-                    break;
+                        Console.Clear();
+                        Console.WriteLine("Тут пока не чего нет\n");
+                        gameManager.ReturnStartMenu();
+                        break;
                     case 51: // Создатель
-                    Console.Clear();
-                    Console.WriteLine("Created by -=Diskein=-\n");
-                    gameManager.ReturnStartMenu();
-                    break;
+                        Console.Clear();
+                        Console.WriteLine("Created by -=Diskein=-\n");
+                        gameManager.ReturnStartMenu();
+                        break;
                     case 48: // Выход
-                    endGame = true;
-                    break;
+                        exitGame = true;
+                        break;
                     default:
-                    break;
+                        break;
                 }
             }
             Console.WriteLine("Haжмитe <Enter> для выхода . . . ");
--- GameStat.cs
+++ GameStat.cs
@@ -5,8 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace _15shkiNew
-{
+{   
     internal class GameStat
     {
+        public GameStat(string txt) 
+        {
+            NickName = txt;
+        }
+        public string NickName { get; }
     }
 }
